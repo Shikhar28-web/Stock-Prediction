a3d86a537034bf6691f1efc523160e68d4c67a55
Shikhar Verma <vermashikhar380@gmail.com>
2025-04-30 13:22:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import yfinance as yf
 from tensorflow.keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
-
+from sklearn.metrics import r2_score
 # If the model is already saved, load it
 model = load_model("new_model.h5")
 
-st.header('Stock Market Predictor')
+st.header('Stock Market Predictor(LSTM)')
 
 stock = st.text_input('Enter Stock Symbol', 'GOOG')
 start = '2012-01-01'
@@ -78,6 +78,8 @@ scale = 1/scaler.scale_
 predict = predict * scale
 y = y * scale
 
+
+
 st.subheader('Original Price vs Predicted Price')
 fig4 = plt.figure(figsize=(8,6))
 plt.plot(predict, 'r', label='Original Price')
@@ -86,5 +88,11 @@ plt.xlabel('Time')
 plt.ylabel('Price(Dollars)')
 plt.legend()
 
-plt.show()
+
+
 st.pyplot(fig4)
+
+accuracy = r2_score(y, predict) * 100
+st.subheader("Model Accuracy (R² Score)")
+st.write(f"Accuracy: {accuracy:.2f}%")
+plt.show()
